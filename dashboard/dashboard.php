1dc8c3809e2e58b1641a6f5b4eccbd6e6e312ab1
KCDeJesus <141986152+KCDeJesus@users.noreply.github.com>
2023-12-09 23:37:00 +0800

    Add files via upload

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -4,18 +4,40 @@
   <title>Login 05</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+     body, h2, h3, button {
+      font-family: 'Poppins', sans-serif;
+    }
+    .btn-info {
+        background-color:#28a745; /* Green color */
+        border-color: #36a550; /* Green color for the border */
+      }
+  </style>
 </head>
 <body> 
   <section class="ftco-section">
     <div class="container">
+      <div class="container">
+        <a href="login.html">
+          <span class="glyphicon glyphicon-log-out"></span> Log out
+        </a>
+    </div>
+    <div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">Main Menu</h2>
+				</div>
+			</div>
       <div class="row justify-content-center">
         <div class="col-md-4">
           <div class="wrap">
-            <div class="img" style="background-image: url(images/kian.jpg);"></div>
+            <div class="img" style="background-image: url(images/3.jpg);"></div>
             <div class="login-wrap p-2 p-md-4 text-center">
               <div class="d-flex">
                 <div class="w-100">
@@ -23,7 +45,9 @@
                 </div>
               </div>
               <div class="form-group">
+              <a href="registration.html">
                 <button type="submit" class="form-control btn btn-primary rounded submit px-2">Paano to pupunta doon</button>
+              </a>
               </div>
               <div class="form-group d-md-flex">
               </div>
@@ -33,7 +57,7 @@
 
         <div class="col-md-4">
           <div class="wrap">
-            <div class="img" style="background-image: url(images/kian.jpg);"></div>
+            <div class="img" style="background-image: url(images/4.jpg);"></div>
             <div class="login-wrap p-2 p-md-4 text-center">
               <div class="d-flex">
                 <div class="w-100">
@@ -41,7 +65,9 @@
                 </div>
               </div>
               <div class="form-group">
+              <a href="add_event.html">
                 <button type="submit" class="form-control btn btn-primary rounded submit px-2">Paano to pupunta doon</button>
+              </a>
               </div>
               <div class="form-group d-md-flex">
               </div>
@@ -51,7 +77,7 @@
 
         <div class="col-md-4">
           <div class="wrap">
-            <div class="img" style="background-image: url(images/kian.jpg);"></div>
+            <div class="img" style="background-image: url(images/5.jpg);"></div>
             <div class="login-wrap p-2 p-md-4 text-center">
               <div class="d-flex">
                 <div class="w-100">
@@ -59,7 +85,9 @@
                 </div>
               </div>
               <div class="form-group">
+              <a href="mng_event.html">
                 <button type="submit" class="form-control btn btn-primary rounded submit px-2">Paano to pupunta doon</button>
+               </a>
               </div>
               <div class="form-group d-md-flex">
               </div>
